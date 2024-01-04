d4858426c2266895120f1276c9dfae4e698afd78
kiplimoboor <kiplimoboor@proton.me>
2024-01-04 17:49:06 +0300

    node depth

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_depth- finds depth of a node
+*
+* @tree: the tree
+*
+* Return: the depth of node
+*/
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	if (!tree->parent)
+	{
+		return (0);
+	}
+
+	return (binary_tree_depth(tree->parent) + 1);
+}
