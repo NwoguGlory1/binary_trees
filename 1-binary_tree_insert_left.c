cce9421f6874a2affa640f6fe6dfb837e6c582b7
kiplimoboor <kiplimoboor@proton.me>
2024-01-04 21:52:40 +0300

    add to left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,12 +10,17 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new = binary_tree_node(parent, value);
+	binary_tree_t *new;
 
-	if (parent == NULL || new == NULL)
+	if (parent == NULL)
 		return (NULL);
 
-	if (parent->left != NULL)
+	new = binary_tree_node(parent, value);
+
+	if (new == NULL)
+		return (NULL);
+
+	if (parent->left)
 	{
 		new->left = parent->left;
 		parent->left->parent = new;
