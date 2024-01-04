2514f677c5328452dfa076e1509fa3d4f999c7d2
NwoguGlory1 <glorynwogu18@gmail.com>
2024-01-04 10:04:53 -0800

    10-binary_tree_depth.c

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,13 +1,11 @@
 #include "binary_trees.h"
 
 /**
-* binary_tree_depth- finds depth of a node
-*
+* binary_tree_depth - finds depth of a node
 * @tree: the tree
 *
-* Return: the depth of node
+* Return: the depth of node or 0 if tree is NULL
 */
-
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	if (!tree->parent)
